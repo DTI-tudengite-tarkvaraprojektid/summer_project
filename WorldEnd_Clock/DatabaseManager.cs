2326e72ab7969562da78ce0f86999dbe2415822a
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2019-06-14 15:24:03 +0300

    Connection files added

---
+++ WorldEnd_Clock/DatabaseManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Xamarin.Forms;
+
+namespace WorldEnd_Clock
+{
+    public class DatabaseManager
+    {
+        public SQLiteConnection dbConnection;
+        public DatabaseManager()
+        {
+            dbConnection = DependencyService.Get<IDBInterface>().CreateConnection();
+        }
+
+        public List<Cards> GetAllCards()
+        {
+            return dbConnection.Query<Cards>("Select * From [Card]");
+        }
+
+        public int SaveCards(Cards aCards)
+
+        {
+            return dbConnection.Insert(aCards);
+        }
+    }
+}
---
+++ WorldEnd_Clock/cards.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace WorldEnd_Clock
+{
+   public class Cards
+    {
+        [PrimaryKey, AutoIncrement]
+        public /* int */ string Id { get; set; }
+        public string Kategooria { get; set; }
+        public string Loodusvarad { get; set; }
+        public string Nimetus { get; set; }
+        public string/* int */ Elustik { get; set; }
+        public string/* int */ Turism { get; set; }
+        public string/* int */ Rahuolu { get; set; }
+        public /* int */ string Skoor { get; set; }
+        public string Kirjeldus { get; set; }
+        public /* int */ string Markide_arv { get; set; }
+
+
+    }
+}

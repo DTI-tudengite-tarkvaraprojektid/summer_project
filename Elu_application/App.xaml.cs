7ae1eae225100c86dd1b907a135bcf1c364b1465
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2019-06-20 16:24:22 +0300

    Updated project

---
+++ Elu_application/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elu_application
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTEyMDM1QDMxMzcyZTMxMmUzMGRJL1dDOXJubWJJamE5bnAwU0lKcEtFY2t4cWppMktNT3JFMTdubXZsN0E9");
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarTextColor = Color.Black
+        };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Elu_application/IDBInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Elu_application
+{
+	public interface IDBInterface
+	{
+		SQLiteConnection CreateConnection();
+	}
+}
